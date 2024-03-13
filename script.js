8990a52e17a28028ac1804c970403fcdcdb42f18
Darine <tagdarinefatma@gmail.com>
2024-03-13 19:35:06 +0100

    add an if statement

--- script.js
+++ script.js
@@ -109,7 +109,18 @@ const renderSongs = (array) => {
 };
 
 const sortSongs = () => {
-  userData?.songs.sort((a, b) => {});
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+  return userData?.songs;
 };
 
 renderSongs(userData?.songs);
